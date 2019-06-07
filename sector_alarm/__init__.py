d21277f9d3438359f4e39cd57c51eab1d4fd3137
Martin Gejke <martin@gejke.se>
2019-06-07 23:47:40 +0200

    Update required asyncsector version

--- sector_alarm/__init__.py
+++ sector_alarm/__init__.py
@@ -17,7 +17,7 @@ DATA_SA = "SECTOR_ALARM"
 _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = []
 
-REQUIREMENTS = ['aiohttp', 'asyncsector>=0.1.4']
+REQUIREMENTS = ['aiohttp', 'asyncsector>=0.1.5']
 
 CONF_EMAIL = 'email'
 CONF_PASSWORD = 'password'
