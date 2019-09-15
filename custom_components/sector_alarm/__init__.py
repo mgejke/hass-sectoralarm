ca0c2c06c8cc28a78b31e2cad1321879fdc2f418
Sebastian Steinmann <sst@eika.no>
2019-09-15 09:04:47 +0200

    #19: Changed the structure to support hacs

--- custom_components/sector_alarm/__init__.py
+++ custom_components/sector_alarm/__init__.py
@@ -41,7 +41,7 @@ CONFIG_SCHEMA = vol.Schema({
             vol.Optional(CONF_CODE_FORMAT, default='^\\d{4,6}$'): cv.string,
             vol.Optional(CONF_THERMOMETERS, default=True): cv.boolean,
             vol.Optional(CONF_ALARM_PANEL, default=True): cv.boolean,
-            vol.Optional(CONF_VERSION, default='v1_1_70'): cv.string
+            vol.Optional(CONF_VERSION, default='v1_1_71'): cv.string
         }),
 },
                            extra=vol.ALLOW_EXTRA)
